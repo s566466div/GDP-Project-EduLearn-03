83f01f2067dfb1e3e3b7154463492e81188aa536
mansa-97 <S567011@nwmissouri.edu>
2024-08-30 22:33:34 -0500

    added login.js file

--- final_submission/demo/app.js
+++ final_submission/demo/app.js
@@ -1,37 +1,42 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Import necessary modules
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
-// Remove or comment out if you are not using errorRouter
-// var errorRouter = require('./routes/error');
+// Import routers
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
+const errorRouter = require('./routes/error'); // Added route for error page
 
-var app = express();
+const app = express();
 
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route setup
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-// Remove or comment out if you are not using errorRouter
-// app.use('/error', errorRouter);
 
-// Catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+// Serve error page
+app.use('/error', errorRouter);
+
+// 404 Error handler: Respond with a JSON message instead of an HTML file
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found' });
 });
 
-// Error handler
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.sendFile(path.join(__dirname, 'views/error.html'));
+// Global Error handler: Catch all other errors and respond with JSON
+app.use((err, req, res, next) => {
+  console.error('Error stack:', err.stack);
+  res.status(err.status || 500).json({ error: 'Internal Server Error' });
 });
 
+// Export the app object for use by the server
 module.exports = app;
--- final_submission/demo/routes/login.js
+++ final_submission/demo/routes/login.js
@@ -1,21 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// GET request for login page
-app.get('/views/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'views', 'login.html'));
-});
-
-// POST request for login form
-router.post('/', function(req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (username === 'admin' && password === 'admin') {
-    res.redirect('/dashboard');
-  } else {
-    res.sendFile(path.join(__dirname, '../views/login.html'));
-  }
+router.get('/views/login', (req, res) => {
+  res.render('login'); // Assuming you are rendering a login page
 });
 
 module.exports = router;
