dc177070a0192dda495e17f57654999808f6e5f7
Bathala <S566466@nwmissouri.edu>
2024-09-01 19:20:36 -0500

    adding backend and front end

--- final_submission/demo/routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/views/login', (req, res) => {
-  res.render('login'); // Assuming you are rendering a login page
-});
-
-module.exports = router;
