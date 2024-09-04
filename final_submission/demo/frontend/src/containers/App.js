fb89b3ee02e647c04f39a61f1a7a305efc2b622b
S564200 <S564200@nwmissouri.edu>
2024-09-04 09:35:18 -0500

    added registration html and css file

--- final_submission/demo/frontend/src/containers/App.js
+++ final_submission/demo/frontend/src/containers/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RegistrationPage from '../pages/registration';
+import LoginPage from '../pages/loginPage';
 
 const App = () => {
   return (
     
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<WelcomePage/>} />
+          <Route path="/signup" element={<RegistrationPage />} />
+          <Route path="/" element={<LoginPage />} />
         
         </Routes>
       </BrowserRouter>
