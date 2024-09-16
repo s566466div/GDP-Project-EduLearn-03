31ae673b97c9fc3a10b34084008995966193078c
mansa-97 <S567011@nwmissouri.edu>
2024-09-15 22:59:44 -0500

    HTML part for Course Dashboard-header,sidebar,layout

--- final_submission/demo/frontend/src/containers/App.js
+++ final_submission/demo/frontend/src/containers/App.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegistrationPage from '../pages/registration';
 import LoginPage from '../pages/loginPage';
+import { CourseProvider } from '../contexts/CourseContext';
+import DashboardPage from '../pages/dashboardPage';
 
 const App = () => {
   return (
-    
+    <CourseProvider>
       <BrowserRouter>
         <Routes>
+
           <Route path="/signup" element={<RegistrationPage />} />
           <Route path="/" element={<LoginPage />} />
-        
+          <Route path="/dashboard" element={<DashboardPage />} />
+
         </Routes>
       </BrowserRouter>
-    
+    </CourseProvider>
   );
- 
 };
 
 export default App;
