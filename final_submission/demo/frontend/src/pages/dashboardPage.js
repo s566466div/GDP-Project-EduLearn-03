cab53606185da8c7919e77ea269a9c36ef59e820
mansa-97 <S567011@nwmissouri.edu>
2024-09-16 08:12:20 -0500

    dashboard.js code

---
+++ final_submission/demo/frontend/src/pages/dashboardPage.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+
+const DashboardPage = () => {
+    return(
+        <Layout> 
+            <h1>Stats for engagement</h1>
+            <h1>Calendar</h1>
+            <h1>Pie chart for enrollment</h1>
+            <h1>Ongoing Courses</h1>
+        </Layout>
+    ); 
+};
+
+export default DashboardPage;
